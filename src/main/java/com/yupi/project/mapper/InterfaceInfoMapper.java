8a3db51319c1be4ffbe6771994655163a7a4e429
skye <1652454160@qq.com>
2024-03-26 16:15:19 +0800

    初始代码

---
+++ src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,15 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.InterfaceInfo;
+
+/**
+ * @Entity generator.domain.InterfaceInfo
+ */
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
